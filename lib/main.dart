dd09b8f2b2156e1de2d97bb0a46a2d945c296d10
Dmytro Babiienko <dmytro.babiienko@gmail.com>
2018-12-28 11:35:10 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:phantom_fjord/src/app.dart';
+
+void main() => runApp(App());
---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:phantom_fjord/src/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
