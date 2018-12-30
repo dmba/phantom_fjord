151f1248b8cf9c876848a1facafe7d29e38623e4
Dmytro Babiienko <dmitry.babienko@gmail.com>
2018-12-31 01:55:53 +0200

    Fix unit tests

---
+++ test/test_module.dart
@@ -0,0 +1,9 @@
+import 'package:get_it/get_it.dart';
+import 'package:phantom_fjord/src/di/app_module.dart';
+
+class TestModule extends AppModule {
+  @override
+  void configure(GetIt get) {
+    super.configure(get);
+  }
+}
